3c2f376df44d2c0ce015ec58aeef34f44e46dd0f
Alysson Sena <alyssonsena566@gmail.com>
2023-10-26 16:18:43 -0300

    exercico 15 ao 18

--- src/exercicojava18.java
+++ src/exercicojava18.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class exercicojava18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n1 = sc.nextInt();
         for (int i = 1;i <= n1; i++ ){
             int primeiro = i * i;
