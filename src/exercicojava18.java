65e25bb5966e1ce5877557d13b5aadddcf5c2020
Alysson Sena <alyssonsena566@gmail.com>
2023-10-26 16:15:21 -0300

    exercico 7 ao 15

---
+++ src/exercicojava18.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class exercicojava18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        for (int i = 1;i <= n1; i++ ){
+            int primeiro = i * i;
+            int Segundo = i * i * i;
+            System.out.printf("%d %d %d%n ", i, primeiro,Segundo);
+        }
+        sc.close();
+
+    }
+}
